92a6b34eb9053c0763af7dec1059d777af57dd31
ryanhihgj-cyber <ryanhihgj@gmail.com>
2026-07-21 08:43:02 -0600

    Refactor speech processing route with Twilio integration

--- speechProcess.js
+++ speechProcess.js
@@ -1,18 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const twilio = require("twilio");
+
+const {
+  processCallIntent
+} = require("./aiProcessing");
+
 router.post("/process", async (req, res) => {
 
-  const speech = req.body.SpeechResult || "";
+  const speech =
+    req.body.SpeechResult || "";
 
-  const response =
-      await processCallIntent(speech);
+  console.log("Caller said:", speech);
+
+  const aiResponse =
+    await processCallIntent(speech);
 
   const twiml =
-      new twilio.twiml.VoiceResponse();
+    new twilio.twiml.VoiceResponse();
 
-  twiml.say(response);
+  twiml.say(
+    {
+      voice: "Polly.Joanna"
+    },
+    aiResponse
+  );
 
-  twiml.redirect("/voice");
+  const gather = twiml.gather({
+    input: "speech",
+    speechTimeout: "auto",
+    action: "/voice/process",
+    method: "POST"
+  });
+
+  gather.say(
+    {
+      voice: "Polly.Joanna"
+    },
+    "Do you need anything else?"
+  );
 
   res.type("text/xml");
-
   res.send(twiml.toString());
+
 });
+
+module.exports = router;
