4942d3be2c940f34008107c011f4b3d8238fc0ff
ryanhihgj-cyber <ryanhihgj@gmail.com>
2026-07-21 08:34:56 -0600

    Update voicerouter.js

--- voicerouter.js
+++ voicerouter.js
@@ -6,22 +6,21 @@ router.post("/", async (req, res) => {
 
   const twiml = new twilio.twiml.VoiceResponse();
 
-  twiml.say(
-    {
-      voice: "Polly.Joanna"
-    },
-    "Welcome to Canna Care. How can I help you today?"
-  );
-
-  twiml.gather({
+  const gather = twiml.gather({
     input: "speech",
     speechTimeout: "auto",
     action: "/voice/process",
     method: "POST"
   });
 
+  gather.say(
+    {
+      voice: "Polly.Joanna"
+    },
+    "Welcome to Canna Care. How can I help you today?"
+  );
+
   res.type("text/xml");
   res.send(twiml.toString());
-});
 
-module.exports = router;
+});
