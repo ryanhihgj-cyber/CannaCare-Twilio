27662eb0bb795a0fb5dea96f49a8a45b0977289b
ryanhihgj-cyber <ryanhihgj@gmail.com>
2026-07-22 10:19:02 -0600

    Refactor voicehandoff.js to use Express router

--- voicehandoff.js
+++ voicehandoff.js
@@ -1,17 +1,29 @@
-exports.handler = function(context, event, callback) {
-    const twiml = new Twilio.twiml.VoiceResponse();
+const express = require("express");
+const router = express.Router();
+const twilio = require("twilio");
 
-    twiml.say(
-        "Please hold while I connect you with a budtender."
-    );
+router.post("/", (req, res) => {
 
-    twiml.dial(
-        {
-            answerOnBridge: true,
-            callerId: "+19702485874"
-        },
-        "+19702485874"
-    );
+  const twiml = new twilio.twiml.VoiceResponse();
 
-    callback(null, twiml);
-};
+  twiml.say(
+    {
+      voice: "Polly.Joanna"
+    },
+    "Please hold while I connect you with a budtender."
+  );
+
+  twiml.dial(
+    {
+      answerOnBridge: true,
+      callerId: "+19702485874"
+    },
+    "+19702485874"
+  );
+
+  res.type("text/xml");
+  res.send(twiml.toString());
+
+});
+
+module.exports = router;
