9b2a069545c121c4f53cbe69b0ea6e5e3e170001
ryanhihgj-cyber <ryanhihgj@gmail.com>
2026-07-22 10:00:15 -0600

    Refactor server.js to update routes and messages

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
-
 const express = require("express");
+
+const voiceRouter = require("./voicerouter");
+const speechProcess = require("./speechProcess");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const voiceRoute = require("./routes/voice");
-
-app.use("/voice", voiceRoute);
+app.use("/voice", voiceRouter);
+app.use("/voice", speechProcess);
 
 app.get("/", (req, res) => {
-  res.send("Canna Care AI Phone Running");
+  res.send("Canna Care Voice AI Running");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+  console.log(`Running on ${PORT}`);
 });
