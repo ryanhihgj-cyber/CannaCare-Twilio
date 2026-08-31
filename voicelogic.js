129345f500e0f870e1c986a9076e99c3a02ca971
ryanhihgj-cyber <ryanhihgj@gmail.com>
2026-08-31 16:40:15 -0600

    Refactor Twilio voice response handling

--- voicelogic.js
+++ voicelogic.js
@@ -1,35 +1,18 @@
 const express = require("express");
-2
 const router = express.Router();
-3
 const twilio = require("twilio");
-4
- 
-5
+
 router.post("/", (req, res) => {
-6
-const twiml = new twilio.twiml.VoiceResponse();
-7
- 
-8
-const connect = twiml.connect();
-9
- 
-10
-connect.stream({
-11
-url: `wss://${process.env.DOMAIN}/voice-stream`
-12
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  const connect = twiml.connect();
+
+  connect.stream({
+    url: `wss://${process.env.DOMAIN}/voice-stream`
+  });
+
+  res.type("text/xml");
+  res.send(twiml.toString());
 });
-13
- 
-14
-res.type("text/xml");
-15
-res.send(twiml.toString());
-16
-});
-17
- 
-18
+
 module.exports = router;
